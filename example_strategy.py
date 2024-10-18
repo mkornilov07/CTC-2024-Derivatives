6ab2b619c6bc200c01db43d568a75fea613a8fa7
Mikhail <mkornilov07@gmail.com>
2024-10-17 22:07:30 -0400

    graph and stuff

--- example_strategy.py
+++ example_strategy.py
@@ -35,7 +35,7 @@ class Strategy:
     self.options = self.options.merge(self.underlying_price_daily, left_on = "day", right_index = True)
     self.options.rename(columns = {"open" : "underlying_price"}, inplace=True)
 
-    self.vol_daily = self.underlying_price_daily.pct_change().rolling(self.DAYS_TO_SKIP, min_periods = self.DAYS_TO_SKIP).std() * np.sqrt(252)
+    self.vol_daily = np.log(self.underlying_price_daily.pct_change()+1).rolling(self.DAYS_TO_SKIP, min_periods = self.DAYS_TO_SKIP).std() * np.sqrt(252)
     self.options["time_to_exp_percentage"] = self.time_to_expiration()
     print("Done with time to expiration")
     call, put = self.black_scholes()
@@ -62,8 +62,8 @@ class Strategy:
     r = 0.03 #  continuously compounded risk-free interest rate (% p.a.), stated as 3%
     q = 0. #  continuously compounded dividend yield (% p.a.), no dividend yield
     t = self.options["time_to_exp_percentage"][self.options.day>self.start_date+timedelta(days=self.DAYS_TO_SKIP+2)].to_numpy()
-    print("AAAAAAAAAAAAAAAAA")
-    print(S.dtype, K.dtype, volatility.dtype, t.dtype)
+    # print("AAAAAAAAAAAAAAAAA")
+    # print(S.dtype, K.dtype, volatility.dtype, t.dtype)
     # S,K,volatility,t = S[10000:], K[10000:], volatility[10000:], t[10000:]
     a=(np.log(S) - np.log(K))
     b=(r - q + np.square(volatility) * 0.5)
@@ -75,7 +75,7 @@ class Strategy:
     d2 = d1 - volatility * np.sqrt(t)
     print("BBBBBBBBBBBBBBBBBB")
     call = S * np.exp(-q * t) * norm.cdf(d1) - K * np.exp(-r * t) * norm.cdf(d2)
-    put = K * np.exp(-r * t) * norm.cdf(-d2) - S * np.exp(-q * t) * norm.cdf(d1)
+    put = K * np.exp(-r * t) * norm.cdf(-d2) - S * np.exp(-q * t) * norm.cdf(-d1)
     return call, put
 
   def time_to_expiration(self) -> float:
